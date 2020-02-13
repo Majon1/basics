5bac9a828cdd51869a9a9460c1ecceb32c8fabbe
Majon1 <mathilda.nynas@gmail.com>
2020-02-13 19:58:41 +0200

    ex.18done

--- part1/calculations/exercise_18/Program.cs
+++ part1/calculations/exercise_18/Program.cs
@@ -8,7 +8,16 @@ namespace exercise_18
     {
 
       // Write your code here:
-
+      Console.WriteLine("Give the first number!");
+      string userInput = Console.ReadLine();
+      int intValue = Convert.ToInt32(userInput);
+      Console.WriteLine("Give the second number!");
+      string userIn = Console.ReadLine();
+      int intV = Convert.ToInt32(userIn);
+      Console.WriteLine("Give the third number!");
+      string userInp = Console.ReadLine();
+      int intVa = Convert.ToInt32(userInp);
+      Console.WriteLine("The sum is " + ((intValue + intV + intVa)));
     }
   }
 }
