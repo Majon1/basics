8a56f223ecc6d9b46927cba6c028bbdad79b593c
Mathilda Nynäs SOFTK20P <mathilda.nynas@centria.fi>
2020-02-14 11:09:58 +0200

    ex.15 done

--- part1/variables/exercise_15/Program.cs
+++ part1/variables/exercise_15/Program.cs
@@ -12,12 +12,17 @@ namespace exercise_15
       string message = Console.ReadLine();
       Console.WriteLine("Give an integer:");
       string message2 = Console.ReadLine();
-
-      
+      int intm = Convert.ToInt32(message2);
+      Console.WriteLine("Give a double:");
+      string doub = Console.ReadLine();
+      double doub2 = Convert.ToDouble(doub);
       Console.WriteLine("Give a boolean:");
+      string message3 = Console.ReadLine();
+      bool booleanValue = System.Convert.ToBoolean(message3);
       Console.WriteLine("Your string: " + message);
       Console.WriteLine("Your integer: " + message2);
-
+      Console.WriteLine("Your double: " + doub2);
+      Console.WriteLine("Your boolean: " + booleanValue);
     }
   }
 }
