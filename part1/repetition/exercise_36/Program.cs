8bc45bc17727921e99282893f5015a5e74ec5f10
Mathilda Nynäs SOFTK20P <mathilda.nynas@centria.fi>
2020-02-14 13:47:58 +0200

    ex.36 done

--- part1/repetition/exercise_36/Program.cs
+++ part1/repetition/exercise_36/Program.cs
@@ -7,7 +7,26 @@ namespace exercise_36
     public static void Main(string[] args)
     {
       // Write your code here:
-
+      while (true)
+      {
+      Console.WriteLine("Give a number:");
+      string UserInput = Console.ReadLine();
+      int number = Convert.ToInt32(UserInput);
+      if (number == 0)
+      {
+        break;
+      }
+      else if (number <= 0)
+      {
+        Console.WriteLine("That is negative");
+      }
+      else if (number > 0)
+      {
+        Console.WriteLine((number * number));
+      }
+      
+      
+      }
     }
   }
 }
