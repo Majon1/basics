ac9ab03a2be56762695ccda45e8a52001dce6ec1
Mathilda Nynäs SOFTK20P <mathilda.nynas@centria.fi>
2020-02-14 10:19:07 +0200

    ex.26 done

--- part1/conditionals/exercise_26/Program.cs
+++ part1/conditionals/exercise_26/Program.cs
@@ -7,7 +7,13 @@ namespace exercise_26
     public static void Main(string[] args)
     {
       // Write your code here:
-
+      Console.WriteLine("Give your age:");
+      string age = Console.ReadLine();
+      int number = Convert.ToInt32(age);
+      if (number < 1900)
+        {
+          Console.WriteLine("You're old"); 
+        }
     }
   }
 }
