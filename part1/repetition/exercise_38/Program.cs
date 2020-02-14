71238ad9802ca74c2296fe4d1cd2ef6bde9e53f1
Majon1 <mathilda.nynas@gmail.com>
2020-02-14 18:49:27 +0200

    ex.38done

--- part1/repetition/exercise_38/Program.cs
+++ part1/repetition/exercise_38/Program.cs
@@ -7,7 +7,22 @@ namespace exercise_38
     public static void Main(string[] args)
     {
       // Write your code here:
-
+    int sum = 0;
+    while (true)
+    {
+      Console.WriteLine("Give a number:");
+      string UserInput = Console.ReadLine();
+      int number = Convert.ToInt32(UserInput);
+      if (number == 0)
+      {
+        Console.WriteLine("Total amount of negative numbers: " + sum);
+        break;
+      }
+      else if (number < 0)
+      {
+        sum = sum + 1;
+      }
+    } 
     }
   }
 }
