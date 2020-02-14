6aff615a6049320bac03981cf7832186f3e1c8a4
Mathilda Nynäs SOFTK20P <mathilda.nynas@centria.fi>
2020-02-14 10:34:20 +0200

    ex.27done

--- part1/conditionals/exercise_27/Program.cs
+++ part1/conditionals/exercise_27/Program.cs
@@ -7,7 +7,17 @@ namespace exercise_27
     public static void Main(string[] args)
     {
       // Write your code here:
-
+      Console.WriteLine("Give a number:");
+      string number = Console.ReadLine();
+      int numberpos = Convert.ToInt32(number);
+      if (numberpos > 0)
+        {
+          Console.WriteLine("It is positive"); 
+        }
+      if (numberpos <= 0)
+        {
+          Console.WriteLine("It is not positive"); 
+        } 
     }
   }
 }
