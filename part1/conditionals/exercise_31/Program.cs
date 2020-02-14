eb03096e431ec56a0c4baee7289b7893b61da9d2
Mathilda Nynäs SOFTK20P <mathilda.nynas@centria.fi>
2020-02-14 12:34:25 +0200

    ex.31 done

--- part1/conditionals/exercise_31/Program.cs
+++ part1/conditionals/exercise_31/Program.cs
@@ -7,7 +7,17 @@ namespace exercise_31
     public static void Main(string[] args)
     {
       // Write your code here:
-
+      Console.WriteLine("Give a number:");
+      string UserInput = Console.ReadLine();
+      int number = Convert.ToInt32(UserInput);
+      if (number % 2 == 0)
+      {
+      Console.WriteLine("It is even.");  
+      }
+      else
+      {
+      Console.WriteLine("It is odd.");
+      } 
     }
   }
 }
