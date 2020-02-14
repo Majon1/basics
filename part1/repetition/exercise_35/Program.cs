d69138dac6e813583c772c38d9ecfd8c1c08d76f
Mathilda Nynäs SOFTK20P <mathilda.nynas@centria.fi>
2020-02-14 13:30:10 +0200

    ex.35 done

--- part1/repetition/exercise_35/Program.cs
+++ part1/repetition/exercise_35/Program.cs
@@ -7,7 +7,16 @@ namespace exercise_35
     public static void Main(string[] args)
     {
       // Write your code here:
-
+      while (true)
+      {
+        Console.WriteLine("Give a number:");
+        string answer = Console.ReadLine();
+        int number = Convert.ToInt32(answer);
+        if (number == 42)
+        {
+          break;
+        }
+      }
     }
   }
 }
