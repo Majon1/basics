e8f5fa36e36de9dbd4fedfd33d8efcbd64e505f6
Mathilda Nynäs SOFTK20P <mathilda.nynas@centria.fi>
2020-02-14 11:22:01 +0200

    ex.29 done

--- part1/conditionals/exercise_29/Program.cs
+++ part1/conditionals/exercise_29/Program.cs
@@ -7,7 +7,24 @@ namespace exercise_29
     public static void Main(string[] args)
     {
       // Write your code here:
-
+      Console.WriteLine("Give the first number!");
+      string nr = Console.ReadLine();
+      int first = Convert.ToInt32(nr);
+      Console.WriteLine("Give the second number!");
+      string nr2 = Console.ReadLine();
+      int second = Convert.ToInt32(nr2);
+      if (first > second)
+        { 
+         Console.WriteLine("The larger number is " + first + "!");
+        }
+      if (first < second)
+        { 
+          Console.WriteLine("The larger number is " + second + "!");
+        } 
+      if (first == second)
+        {
+          Console.WriteLine("They are equal!");
+        }
     }
   }
 }
