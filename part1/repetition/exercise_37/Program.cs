ac99eb4f28a2a7e53c0cfb5aa613633f5a24bfab
Majon1 <mathilda.nynas@gmail.com>
2020-02-14 18:42:20 +0200

    ex.37 done

--- part1/repetition/exercise_37/Program.cs
+++ part1/repetition/exercise_37/Program.cs
@@ -8,6 +8,23 @@ namespace exercise_37
     {
       int sum = 0;
       // Write your code here:
+      while (true)
+      {
+       Console.WriteLine("Give a number:");
+       string UserInput = Console.ReadLine();
+       int number = Convert.ToInt32(UserInput);
+       if (number == 0)
+       {
+         Console.WriteLine("Total amount of numbers: " + sum);
+         break;
+       } 
+       else
+       {
+         sum = sum + 1;
+       }
+      
+
+      }
 
     }
   }
