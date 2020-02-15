259fe356a848b291b2f241b1996d898cf286afa0
Majon1 <mathilda.nynas@gmail.com>
2020-02-15 15:53:05 +0200

    ex.52 done

--- part2/methods/exercise_52/Program.cs
+++ part2/methods/exercise_52/Program.cs
@@ -7,10 +7,19 @@ namespace exercise_52
     public static void Main(String[] args)
     {
       // Call your method here:
-
+      int number = Convert.ToInt32(5);
+      PrintFromNumberToOne (number);
     }
 
     // Write your method here:
-
+    public static void PrintFromNumberToOne(int number)
+    {
+    int i = 1;
+    while (i <= number)
+    {
+      Console.WriteLine(number);
+      number = number - 1;
+    }
+    }
   }
 }
