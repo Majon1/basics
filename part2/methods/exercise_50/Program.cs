58c841465a64e9b0290a758fa4fb5f052b9b7c4e
Majon1 <mathilda.nynas@gmail.com>
2020-02-15 15:46:39 +0200

    exercise50-51 done

--- part2/methods/exercise_50/Program.cs
+++ part2/methods/exercise_50/Program.cs
@@ -7,12 +7,30 @@ namespace exercise_50
     public static void Main(string[] args)
     {
       // Call your method here:
- 
+      int i = 1;
+      
+      Console.WriteLine("How many times?");
+      string UserInput = Console.ReadLine();
+      int number = Convert.ToInt32(UserInput);
+      
+      PrintPhrase(i, number);
+  
     }
 
     // Write your method here:
-    public static void PrintPhrase() {
+    public static void PrintPhrase(int i, int number)
+    {
+    
+    
+    while (i <= number)
+      { 
+      Console.WriteLine("In a hole in the ground there lived a method");
+      i = i + 1;
+      }
+     }
 
     }
-  }
 }
+
+    
+
--- part2/methods/exercise_51/Program.cs
+++ part2/methods/exercise_51/Program.cs
@@ -6,12 +6,22 @@ namespace exercise_51
   {
     public static void Main(String[] args)
     {
+      
       // Call your method here:
-
+    int number = Convert.ToInt32(5);
+    PrintUntilNumber(number);
 
     }
 
     // Write your method here:
-
+    public static void PrintUntilNumber(int number)
+    {
+    int i = 1;
+    while (i <= number)
+    {
+      Console.WriteLine(i);
+      i++;
+    }
+    }
   }
 }
