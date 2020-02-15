369fc41a39b8f8aafac44867e3584532438ed2d5
Majon1 <mathilda.nynas@gmail.com>
2020-02-15 16:10:00 +0200

    ex.53 why

--- part2/methods/exercise_53/Program.cs
+++ part2/methods/exercise_53/Program.cs
@@ -7,11 +7,17 @@ namespace exercise_53
     public static void Main(String[] args)
     {
       // Call your method here:
-
+      int numerator = 9;
+      int denominator = 7;
+      Division(numerator, denominator); 
 
     }
 
     // Write your method here:
-
+      public static void Division(int numerator, int denominator)
+      {
+       
+        Console.WriteLine((double)numerator / denominator);
+      }
   }
 }
