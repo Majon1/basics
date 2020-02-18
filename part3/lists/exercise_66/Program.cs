61b3b039a9ddfb0643ab20b35d2f73bdea0d015c
Majon1 <mathilda.nynas@gmail.com>
2020-02-18 17:19:21 +0200

    ex.66 done

--- part3/lists/exercise_66/Program.cs
+++ part3/lists/exercise_66/Program.cs
@@ -6,17 +6,20 @@ namespace exercise_66
   class Program
   {
     public static void Main(string[] args)
-    {
+    { int i = 0;
       List<string> list = new List<string>();
       while (true)
       {
         string input = Console.ReadLine();
         if (input == "")
         {
+          Console.WriteLine(list[i - 1]);
           break;
         }
         list.Add(input);
+        i++;
       }
+
 
     }
   }
