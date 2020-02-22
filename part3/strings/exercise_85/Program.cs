3b796f8d09b9c367ef0cc352398cf91896136965
Majon1 <mathilda.nynas@gmail.com>
2020-02-22 14:09:33 +0200

    ex. 85 done

--- part3/strings/exercise_85/Program.cs
+++ part3/strings/exercise_85/Program.cs
@@ -7,6 +7,37 @@ namespace exercise_85
   {
     public static void Main(string[] args)
     {
+      Console.WriteLine("Enter username:");
+      string username = Console.ReadLine();
+      Console.WriteLine("Enter password:");
+      string password = Console.ReadLine();
+
+      if (username == "alex")
+      {
+        if (password == "sunshine")
+        {
+          Console.WriteLine("You have successfully logged in!");
+        }
+        else
+        {
+          Console.WriteLine("Incorrect username or password!");
+        }
+      }
+      else if (username == "emma")
+      {
+        if (password == "haskell")
+        {
+          Console.WriteLine("You have successfully logged in!");
+        }
+        else
+        {
+          Console.WriteLine("Incorrect username or password!");
+        }
+      }
+      else
+      {
+        Console.WriteLine("Incorrect username or password!");
+      }
 
 
     }
