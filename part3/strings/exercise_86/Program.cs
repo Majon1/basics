55c46c9f01f15ff8d28112db83a52ed8b2a275e7
Majon1 <mathilda.nynas@gmail.com>
2020-02-22 19:31:59 +0200

    86,88,89 done

--- part3/strings/exercise_86/Program.cs
+++ part3/strings/exercise_86/Program.cs
@@ -7,8 +7,23 @@ namespace exercise_86
   {
     public static void Main(string[] args)
     {
-
-
+      while (true)
+      { 
+        string input = Console.ReadLine();
+        
+        if (input == "")
+        {
+          break;
+        }
+      
+        string[] pieces = input.Split(" ");
+        int index = 0;
+        while (index < pieces.Length)
+        {
+        Console.WriteLine(pieces[index]);
+        index++;
+        }
+      }
     }
   }
 }
--- part3/strings/exercise_89/Program.cs
+++ part3/strings/exercise_89/Program.cs
@@ -7,7 +7,18 @@ namespace exercise_89
   {
     public static void Main(string[] args)
     {
-
+        while (true)
+      { 
+        string input = Console.ReadLine();
+        
+        if (input == "")
+        {
+          break;
+        }
+        string[] pieces = input.Split(" "); 
+        int index = pieces.Length;
+        Console.WriteLine(pieces[index - 1]);
+      }
 
     }
   }
