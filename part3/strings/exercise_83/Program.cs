8e3bceab50365d67dc7021331d60a541c7eda06b
Majon1 <mathilda.nynas@gmail.com>
2020-02-22 13:53:44 +0200

    ex. 83 done

--- part3/strings/exercise_83/Program.cs
+++ part3/strings/exercise_83/Program.cs
@@ -6,11 +6,11 @@ namespace exercise_83
   class Program
   {
     public static void Main(string[] args)
-    {
-
-
+    { Console.Write("Give a word: ");
+      string userinput = Console.ReadLine();
+      Console.Write(userinput + userinput + userinput);
+      Console.WriteLine("");
     }
-
   }
 }
 
