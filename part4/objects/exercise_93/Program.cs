b2a4b884d4e2d95e530d9d21f00e84f7e4dc8567
Majon1 <mathilda.nynas@gmail.com>
2020-02-23 22:01:24 +0200

    ex.93 done

--- part4/objects/exercise_93/Program.cs
+++ part4/objects/exercise_93/Program.cs
@@ -8,7 +8,9 @@ namespace exercise_93
     {
       // DO NOT TOUCH THE OTHER FILE!
       // Do your code here!
-
+      Account secondAccount = new Account("second account", 100.00);
+      secondAccount.Deposit(20);
+      Console.WriteLine(secondAccount.balance);
     }
   }
 }
