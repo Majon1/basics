3511624a411aa03d878c268d5dbdcbc5f6990533
Majon1 <mathilda.nynas@gmail.com>
2020-02-23 15:22:34 +0200

    fix later

--- part3/strings/exercise_91/Program.cs
+++ part3/strings/exercise_91/Program.cs
@@ -7,6 +7,19 @@ namespace exercise_91
   {
     public static void Main(string[] args)
     {
+     
+      while (true)
+      {
+        string input = Console.ReadLine();
+        if (input == "")
+        {
+          break;
+        }
+        string[] pieces = input.Split(",");
+        string nameLength = pieces[0].Length;
+        
+      }
+      Console.WriteLine("Age of the oldest: " + nameLength);
 
     }
   }
