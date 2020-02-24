bff4d16060d6312dc5fedd45afcc045746505365
Mathilda Nynäs SOFTK20P <mathilda.nynas@centria.fi>
2020-02-24 14:19:11 +0200

    ex.112 done

--- part4/objectlist/exercise_112/Program.cs
+++ part4/objectlist/exercise_112/Program.cs
@@ -8,9 +8,34 @@ namespace exercise_112
     public static void Main(string[] args)
     {
       // IMPLEMENT YOUR CODE IN HERE!
+      List<TelevisionProgram> list = new List<TelevisionProgram>();
+      while (true)
+      {
+        Console.Write("Name: ");
+        string name = Console.ReadLine();
+        if (name == "")
+        {
+          break;
+        }
 
+        Console.Write("Duration: ");
+        int duration = Convert.ToInt32(Console.ReadLine());
+     
+      list.Add(new TelevisionProgram (name, duration));
 
+      }
+      Console.WriteLine();
+      Console.Write("Program's maximum duration? ");
+      int maxtime = Convert.ToInt32(Console.ReadLine());
+  
+    foreach (TelevisionProgram program in list)
+    {
+      if (program.duration <= maxtime)
+      {
+        Console.WriteLine(program.name + ", " + program.duration + " minutes");
+      }
     }
+  }
   }
 }
 
