30ad069cc0e6caf4bc66a4166a69876f6d285e15
Mathilda Nynäs SOFTK20P <mathilda.nynas@centria.fi>
2020-02-24 13:27:54 +0200

    ex. 108 done

---
+++ part4/objects/exercise_108/PaymentCard.cs
@@ -0,0 +1,32 @@
+using System;
+
+namespace exercise_108
+{
+    public class PaymentCard
+    {
+        private double balance;
+
+        public PaymentCard(double openingBalance)
+        {
+            this.balance = openingBalance;
+        }
+        public void EatLunch()
+        {
+            if (this.balance >= 10.60)
+            {
+           this.balance = this.balance - 10.60;
+            }
+        }
+        public void DrinkCoffee()
+        {
+            if (this.balance >= 2.00)
+            {
+           this.balance = this.balance - 2.00;
+            }
+        }
+        public override string ToString()
+        {
+            return "The card has a balance of " + this.balance + " euros";
+        }
+    }
+}
