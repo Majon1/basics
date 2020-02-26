39fc7045fea6b035bc62b2c1294098dbb51e7794
Majon1 <mathilda.nynas@gmail.com>
2020-02-26 13:57:47 +0200

    ex.114 done

--- part4/files/exercise_115/Program.cs
+++ part4/files/exercise_115/Program.cs
@@ -6,7 +6,18 @@ namespace exercise_115
   {
     public static void Main(string[] args)
     {
+      while (true)
+      {
+      string input = Console.ReadLine();
 
+      if (input == "end")
+        {
+          break;
+        }
+         int userinput = Convert.ToInt32(input);
+         int cubed = userinput * userinput * userinput;
+         Console.WriteLine(cubed);
+      }  
     }
   }
 }
