d0879efc4d7dca4f5f4fe4ca3feadad6a59ce731
Majon1 <mathilda.nynas@gmail.com>
2020-02-26 13:53:25 +0200

    ex.114 done

--- part4/files/exercise_114/Program.cs
+++ part4/files/exercise_114/Program.cs
@@ -5,8 +5,19 @@ namespace exercise_114
     class Program
     {
         public static void Main(string[] args)
-        {
+        {   int amount = 0;
+            while (true)
+            {
+                string input = Console.ReadLine();
 
+                if (input == "end")
+                {
+                    break;
+                }
+                amount++;  
+            }
+            Console.WriteLine(amount);
+            }
         }
     }
-}
+
