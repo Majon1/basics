6f8135cd328410838bd0d9f299977262abe6422b
Majon1 <mathilda.nynas@gmail.com>
2020-02-26 14:15:34 +0200

    ex.117 done

--- part4/files/exercise_117/Program.cs
+++ part4/files/exercise_117/Program.cs
@@ -8,7 +8,10 @@ namespace exercise_117
     public static void Main(string[] args)
     {
       // Ask the user for the file name and print the content of the file
-
+      Console.WriteLine("Which file should have its contents printed?");
+      string which = Console.ReadLine();
+      string text = File.ReadAllText(which);
+      Console.WriteLine(text);  
       
     }
   }
