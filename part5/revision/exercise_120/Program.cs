5f24469a66ef7b8a3ddabbd8a055caeb59c5209f
Majon1 <mathilda.nynas@gmail.com>
2020-02-27 10:26:35 +0200

    ex. 120 done

--- part5/revision/exercise_120/Program.cs
+++ part5/revision/exercise_120/Program.cs
@@ -7,7 +7,8 @@ namespace exercise_120
     public static void Main(string[] args)
     {
       // You can test your cube here.
-      
+      Cube firstCube = new Cube(8);
+      Console.WriteLine(firstCube);
     }
   }
 }
