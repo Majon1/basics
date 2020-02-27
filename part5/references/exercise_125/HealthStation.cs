031aa7f9e7c019df422360de4993be92096317aa
Majon1 <mathilda.nynas@gmail.com>
2020-02-27 14:58:33 +0200

    ex.125 done

--- part5/references/exercise_125/HealthStation.cs
+++ part5/references/exercise_125/HealthStation.cs
@@ -4,18 +4,24 @@ namespace exercise_125
   {
     public int weighings { get; private set; }
 
+    public HealthStation()
+    {
+      this.weighings = 0;
+    }
     // Create a constructor here, if needed
 
     public int Weigh(Person person)
     {
+      weighings++;
+      return person.weight;
       // return the weight of the person passed as the parameter
       // incrase weighings
-      return 0;
     }
 
     public void Feed(Person person)
     {
       // Increse the weight of the person with one kilogram
+      person.weight++;
     }
   }
 }
