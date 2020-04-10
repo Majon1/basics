7cd18d30884963598c6be21f1196264e8b42ff0d
Majon1 <mathilda.nynas@gmail.com>
2020-04-10 13:12:40 +0300

    not done

---
+++ project_examples/NewTypes/src/NewTypes/NewTypesTests/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace NewTypesTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
