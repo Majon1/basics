eef3727e73b4bdb456f8ae2e6e5586e979487312
Majon1 <mathilda.nynas@gmail.com>
2020-04-14 12:33:05 +0300

    ex 145 done

--- part8/exercise_145/src/Exercise/VehicleRegistry.cs
+++ part8/exercise_145/src/Exercise/VehicleRegistry.cs
@@ -6,29 +6,67 @@ namespace Exercise
   public class VehicleRegistry
   {
     private Dictionary<LicensePlate, string> owners = new Dictionary<LicensePlate, string>();
+    public VehicleRegistry()
+    {
+      this.owners = new Dictionary<LicensePlate, string>();
+    }
     public bool Add(LicensePlate licensePlate, string owner)
     {
-      return false;
+    if (!this.owners.ContainsKey(licensePlate))
+      {
+      this.owners.Add(licensePlate, owner);
+      return true;
+      }
+     else 
+     {
+       return false;
+     } 
     }
 
     public string Get(LicensePlate licensePlate)
     {
-      return "No such license";
+    if (this.owners.ContainsKey(licensePlate))
+      {
+        return this.owners[licensePlate];
+      }
+    else
+      {
+      return "No such licenseplate";
+      }
     }
 
     public bool Remove(LicensePlate licensePlate)
     {
-      return false;
+      if (this.owners.ContainsKey(licensePlate))
+      {
+        this.owners.Remove(licensePlate);
+        return true;
+      } 
+      else
+      {
+      return false; 
+      }
     }
 
     public void PrintLicensePlates()
     {
-      
+      foreach (KeyValuePair<LicensePlate, string> kvp in owners)
+      {
+        Console.WriteLine("{0}", kvp.Key);
+      }
     }
 
     public void PrintOwners()
-    {
-
+    { 
+      List <string>printedOwner = new List<string>();
+      foreach (KeyValuePair<LicensePlate, string> kvp in this.owners)
+      {
+        if (!printedOwner.Contains(kvp.Value))
+        {
+        Console.WriteLine(kvp.Value);
+        }
+        printedOwner.Add(kvp.Value);
+      }
     }
   }
 }
