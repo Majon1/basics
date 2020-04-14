ffba852d3ff05eff4218282f8f2ac0da9fff7c21
Majon1 <mathilda.nynas@gmail.com>
2020-04-14 11:41:12 +0300

    ex 144 done

--- part8/exercise_144/src/Exercise/LicensePlate.cs
+++ part8/exercise_144/src/Exercise/LicensePlate.cs
@@ -19,12 +19,22 @@ namespace Exercise
 
     public override bool Equals(object compared)
     {
+      if (this == compared)
+      {
+        return true;
+      }
+      if ((compared == null) || !this.GetType().Equals(compared.GetType()))
+      {
       return false;
+      }
+      
+     LicensePlate comparedNumber = (LicensePlate)compared;
+     return this.country == comparedNumber.country && this.liNumber == comparedNumber.liNumber;
     }
 
     public override int GetHashCode()
     {
-      return -1;
+    return this.country.GetHashCode() + this.liNumber.GetHashCode();
     }
   }
 }
