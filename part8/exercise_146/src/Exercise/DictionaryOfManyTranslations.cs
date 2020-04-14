0f3436d8476e2510d6fc40a48eab0fd2e72cb36a
Majon1 <mathilda.nynas@gmail.com>
2020-04-14 14:16:30 +0300

    exercise 146 done

--- part8/exercise_146/src/Exercise/DictionaryOfManyTranslations.cs
+++ part8/exercise_146/src/Exercise/DictionaryOfManyTranslations.cs
@@ -4,20 +4,34 @@ namespace Exercise
 {
   public class DictionaryOfManyTranslations
   {
-
+    private Dictionary<string, List<string>> register; 
+   public DictionaryOfManyTranslations()
+   {
+     this.register = new Dictionary<string, List<string>>();
+   }
     public void Add(string word, string translation)
     {
-
+      if (!this.register.ContainsKey(word))
+      {
+        this.register.Add(word, new List<string>());
+      }
+      this.register[word].Add(translation);
     }
 
     public List<string> Translate(string word)
     {
+      if (this.register.ContainsKey(word))
+      {
+        return this.register[word];
+      }
+      else
+      {
       return new List<string>();
+      }
     }
-
     public void Remove(string word)
     {
-
+    this.register.Remove(word);
     }
   }
 }
