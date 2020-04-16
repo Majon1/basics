644209b6d6f1b55e5fd48236b65f274de4ae2011
Majon1 <mathilda.nynas@gmail.com>
2020-04-16 16:19:13 +0300

    ex 149 done

--- part9/exercise_149/src/Exercise/People/Person.cs
+++ part9/exercise_149/src/Exercise/People/Person.cs
@@ -1,6 +1,20 @@
 namespace Exercise
 {
+  using System;
   public class Person
   {
+    public string name { get; set; }
+    public string adress { get; set; }
+
+    public Person(string name, string adress)
+    {
+      this.name = name;
+      this.adress = adress;
+    }
+
+    public override string ToString()
+    {
+      return this.name + ", " + this.adress;
+    } 
   }
 }
--- part9/exercise_149/src/Exercise/Program.cs
+++ part9/exercise_149/src/Exercise/Program.cs
@@ -6,7 +6,24 @@ namespace Exercise
   {
     public static void Main(string[] args)
     {
+      Person adaA = new Person("Ada Lovelace", "24 Maddox St. London W1S 2QN");
+      Person eskoO = new Person("Esko Ukkonen", "Mannerheimintie 15 00100 Helsinki");
+      Console.WriteLine(adaA);
+      Console.WriteLine(eskoO);
 
+      Console.WriteLine();
+
+      Student ollie = new Student("Ollie", "6381 Hollywood Blvd. Los Angeles 90028");
+      Console.WriteLine(ollie);
+      ollie.Study();
+      Console.WriteLine(ollie);
+
+      Console.WriteLine();
+
+      Teacher ada = new Teacher("Ada Lovelace", "24 Maddox St. London W1S 2QN", 1200);
+      Teacher esko = new Teacher("Esko Ukkonen", "Mannerheimintie 15 00100 Helsinki", 5400);
+      Console.WriteLine(ada);
+      Console.WriteLine(esko);
     }
   }
 }
