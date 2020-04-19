81486af31e508846b4106783b7bb80b41ca01d6d
Majon1 <mathilda.nynas@gmail.com>
2020-04-19 13:30:02 +0300

    ex 154 done

--- part9/exercise_154/src/Exercise/Herd/Herd.cs
+++ part9/exercise_154/src/Exercise/Herd/Herd.cs
@@ -1,24 +1,36 @@
 namespace Exercise
-{
+{ using System;
   using System.Collections.Generic;
   public class Herd : IMovable
   {
+    private List<IMovable> herd;
 
     public Herd()
     {
+      this.herd = new List<IMovable>();
     }
 
-    public void AddToHerd(IMovable m)
+    public void AddToHerd(IMovable movable)
     {
+      this.herd.Add(movable);
     }
 
     public void Move(int dx, int dy)
     {
+      foreach (IMovable movable in herd)
+      {
+        movable.Move(dx,dy);
+      }
     }
 
     public override string ToString()
     {
-      return "";
+      string position = "";
+      foreach (IMovable movable in herd)
+      {
+        position += movable.ToString() + "\n";
+      }
+      return position;
     }
   }
 }
