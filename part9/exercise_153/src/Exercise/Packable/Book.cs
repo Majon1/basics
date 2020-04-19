6c8487d4013d9233b75d82d1a478e51ea6c4454c
Majon1 <mathilda.nynas@gmail.com>
2020-04-19 12:58:46 +0300

    ex 153 done

--- part9/exercise_153/src/Exercise/Packable/Book.cs
+++ part9/exercise_153/src/Exercise/Packable/Book.cs
@@ -2,19 +2,26 @@ namespace Exercise
 {
   public class Book : IPackable
   {
+    public string author;
+    public string book;
+    public int year;
+    public int weight;
 
-    public Book(string s, string s2, int i)
+    public Book(string author, string book, int year)  //author, name of book, publication year
     {
+      this.author = author;
+      this.book = book;
+      this.year = year;
+      this.weight = 1;
     }
 
     public int Weight()
     {
-      return 0;
+      return this.weight;
     }
-
     public override string ToString()
     {
-      return "";
+      return this.author + ": " + this.book + " (" + this.year + ")";
     }
   }
 }
--- part9/exercise_153/src/Exercise/Packable/Box.cs
+++ part9/exercise_153/src/Exercise/Packable/Box.cs
@@ -1,23 +1,39 @@
+using System;
+using System.Collections.Generic;
+
 namespace Exercise
 {
   public class Box : IPackable
   {
-    public Box(int i)
+    private List<IPackable> list;
+    private int maxCapacity;
+    public Box(int maxCapacity)
     {
+      this.maxCapacity = maxCapacity;
+      this.list = new List<IPackable>();
     }
 
     public void Add(IPackable item)
     {
+      if (Weight() + item.Weight() <= this.maxCapacity)
+      {
+        list.Add(item);
+      }
     }
 
     public int Weight()
     {
-      return 0;
+      int weightSum = 0;
+      foreach (IPackable item in list)
+      {
+        weightSum = weightSum + item.Weight();
+      }
+      return weightSum;
     }
 
     public override string ToString()
     {
-      return "";
+      return list.Count + " items, total weight " + Weight() + " kg";
     }
   }
 }
