dea4808e7f72d99428d25c02b01ff05171b2207e
Majon1 <mathilda.nynas@gmail.com>
2020-04-19 12:25:11 +0300

    ex 151 done

--- part9/exercise_152/src/Exercise/Tacos/TripleTacoBox.cs
+++ part9/exercise_152/src/Exercise/Tacos/TripleTacoBox.cs
@@ -2,15 +2,23 @@ namespace Exercise
 {
   public class TripleTacoBox : ITacoBox
   {
-
+    public int tacos;
+    public TripleTacoBox()
+    {
+     this.tacos = 3;
+    }
+    
     public int TacosRemaining()
     {
-      return 0;
+      return this.tacos;
     }
 
     public void Eat()
     {
-
+      if (this.tacos > 0)
+      {
+        this.tacos = this.tacos -1;
+      }
     }
   }
 }
