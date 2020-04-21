bcd7ff41bc65d9615ca554cb7b5ff0bc9ee0e959
Majon1 <mathilda.nynas@gmail.com>
2020-04-21 10:39:11 +0300

    exercise 158 done

--- part10/exercise_158/src/Exercise/UserInterfaces/TextInterface.cs
+++ part10/exercise_158/src/Exercise/UserInterfaces/TextInterface.cs
@@ -8,10 +8,30 @@ namespace Exercise
     private List<Book> books;
     public TextInterface()
     {
-    }
+      this.books = new List<Book>();
+    } 
 
     public void Start()
     {
+      while (true)
+      {
+        Console.WriteLine("Input the name of the book, if empty stops:");
+        string name = Console.ReadLine();
+        if (name == "")
+        {
+          break;
+        }
+        Console.WriteLine("Input the age recommendation:");
+        int age = Convert.ToInt32(Console.ReadLine());
+        this.books.Add(new Book(name, age));
+      }
+      
+      Console.WriteLine(this.books.Count + " books in total.");
+      Console.WriteLine();
+      this.books.Sort();
+      Console.WriteLine("Books:");
+      this.books.ForEach(Console.WriteLine);
+
     }
   }
 }
