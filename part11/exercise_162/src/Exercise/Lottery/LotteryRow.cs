0df226a1cfbc06d99403c99321bf4a0d7fd2b521
Majon1 <mathilda.nynas@gmail.com>
2020-04-23 13:39:53 +0300

    ex 162 not done

--- part11/exercise_162/src/Exercise/Lottery/LotteryRow.cs
+++ part11/exercise_162/src/Exercise/Lottery/LotteryRow.cs
@@ -1,34 +1,58 @@
 namespace Exercise
 {
-  using System;
-  using System.Collections.Generic;
+    using System;
+    using System.Collections.Generic;
 
-  public class LotteryRow
-  {
-    private List<int> numbers;
-
-    public LotteryRow()
+    public class LotteryRow
     {
-      this.RandomizeNumbers();
-    }
+        private List<int> numbers;
 
-    public List<int> Numbers()
-    {
-      return this.numbers;
-    }
+        public LotteryRow()
+        {
+            this.RandomizeNumbers();
+        }
 
-    public bool ContainsNumber(int number)
-    {
-      // Tests whether the number is already among the randomized numbers
-      return false;
-    }
+        public List<int> Numbers()
+        {
+            return this.numbers;
+        }
 
-    public void RandomizeNumbers()
-    {
-      // initialize the list for numbers
-      this.numbers = new List<int>();
-      // Implement the randomization of the numbers by using the method ContainsNumber() here
-    }
+        public bool ContainsNumber(int number)
+        {
 
-  }
+            if (this.numbers.Contains(number))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+            // Tests whether the number is already among the randomized numbers
+
+        }
+
+        public void RandomizeNumbers()
+        {
+            Random number = new Random();
+            // initialize the list for numbers
+            this.numbers = new List<int>();
+
+            for (int i = 1; i <= 8; i++)
+            {
+                int randomNumber = number.Next(40);
+               if(!(this.numbers.Contains(randomNumber)))
+                {
+                  this.numbers.Add(randomNumber);
+                  i++;
+                }
+                
+        
+            } 
+            this.numbers.Sort();
+            // Implement the randomization of the numbers by using the method ContainsNumber() here
+        }
+
+    }
 }
