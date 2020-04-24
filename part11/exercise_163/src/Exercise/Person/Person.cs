2c606c64202dbafaad6e655b0490a8ef92a95f79
Majon1 <mathilda.nynas@gmail.com>
2020-04-24 09:28:29 +0300

    ex 163 done

--- part11/exercise_163/src/Exercise/Person/Person.cs
+++ part11/exercise_163/src/Exercise/Person/Person.cs
@@ -9,6 +9,14 @@ namespace Exercise
 
     public Person(string name, int age)
     {
+      if (name == null ||  name.Length == 0 || name.Length > 40)
+      {
+        throw new ArgumentException("Name can't be null or over 40 characters.");
+      }
+      if (age <= 0 || age >= 120)
+      {
+        throw new ArgumentException("Age must be between 0-120 years");
+      }
       this.name = name;
       this.age = age;
     }
