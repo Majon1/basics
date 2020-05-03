b77092e3fa65d560814191d0bfc02889d01bc38a
Majon1 <mathilda.nynas@gmail.com>
2020-05-03 14:05:15 +0300

    merge conflict fix

--- part11/exercise_161/src/Exercise/Dice/Die.cs
+++ part11/exercise_161/src/Exercise/Dice/Die.cs
@@ -19,15 +19,6 @@ namespace Exercise
         return randomNumber;
       // generate a random number which may be any number
       // between one and the number of faces, and then return it
-<<<<<<< HEAD
-=======
-      int cast = random.Next(1, this.numberOfFaces + 1);
-
-      // numberOfFaces = 6
-      // Next(1,6)
-      // between 1 and 5
-      return cast;
->>>>>>> c41c326b51267b8a870cd7add7774b5bfb1e0b12
     }
   }
 }
